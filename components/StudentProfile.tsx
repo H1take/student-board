e5f057278e34e4a41ca63405e2b97d4998f6c5ec
Dmitriy <h1takedev@gmail.com>
2024-06-18 23:27:22 +0300

    fix: adaptive layout

--- components/StudentProfile.tsx
+++ components/StudentProfile.tsx
@@ -1,23 +1,30 @@
 'use client';
 import React from 'react';
-import {Flex, Text} from "@chakra-ui/react";
-import {IStudent} from "@/components/Home";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { IStudent } from "@/components/Home";
 import Image from "next/image";
 
 const StudentProfile = ({ student }: { student: IStudent | undefined }) => {
-    console.log(student)
-
-    return(
-        <Flex mt={['50px', '100px']} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+    return (
+        <Flex
+            mt={['50px', '100px']}
+            padding={'20px'}
+            flexDirection={'column'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            overflow={'hidden'}
+        >
             {student?.photo &&
-                <Image style={{borderRadius: '0.5rem'}} width={300} height={300} src={student?.photo} alt={student?.name}/>
+                <Image style={{ borderRadius: '0.5rem' }} width={300} height={300} src={student?.photo} alt={student?.name} />
             }
-            <Text fontSize={'24px'} as={'b'}>
+            <Text fontSize={'24px'} as={'b'} textAlign={'center'}>
                 {student?.name}
             </Text>
-            <Text fontSize={'18px'} width={['370px', '1500px']} whiteSpace={'pre'} fontWeight={500} mb={'50px'}>
-                {student?.description}
-            </Text>
+            <Box width={['90%', '80%', '70%', '60%']} maxWidth={'1500px'} overflow={'hidden'}>
+                <Text fontSize={'18px'} whiteSpace={'pre-wrap'} fontWeight={500} mb={'50px'}>
+                    {student?.description}
+                </Text>
+            </Box>
         </Flex>
     )
 };
